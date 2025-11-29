e078f4c1a0a9f7cb9db384798ded6330a60e74b2
sidsuchak <mahimayande21@gmail.com>
2025-11-29 17:38:04 +0530

    second commit

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "h1",
   { id: "heading" },
   "My name is Sid2.0"
 );
-console.log(heading, "HEADING");
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root, "ROOT");
+
 root.render(heading);
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Siddharth React
+
+## npm run dev - Start Development server
+
+## npm run build - Production ready code
